088ac50cacaea02ab7c7eea49cc7f384a7c4cbb6
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-07-07 11:00:34 +0200

    migrate file done

--- backend/migrate.js
+++ backend/migrate.js
@@ -1,28 +1,39 @@
-require('dotenv').config();
-
 const fs = require('fs');
 const mysql = require('mysql2/promise');
+const path = require('path');
+
+require('dotenv').config();
+
+const migrationsDir = path.resolve(`${__dirname}/../db/`);
 
 const migrate = async () => {
-  const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
+  const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+
+  const migrations = fs.readdirSync(migrationsDir);
 
   const connection = await mysql.createConnection({
+    database: DB_NAME,
     host: DB_HOST,
+    port: DB_PORT,
     user: DB_USER,
     password: DB_PASSWORD,
-    port: DB_PORT,
     multipleStatements: true,
   });
 
-  await connection.query(`drop database if exists ${DB_NAME}`);
-  await connection.query(`create database ${DB_NAME}`);
-  await connection.query(`use ${DB_NAME}`);
-
-  const sql = fs.readFileSync('../db/init.sql', 'utf8');
-
-  await connection.query(sql);
-
-  connection.end();
+  Promise.all(
+    migrations.map((migrationFile) =>
+      connection.query(
+        fs.readFileSync(`${migrationsDir}/${migrationFile}`).toString()
+      )
+    )
+  )
+    .then(() => {
+      console.log('All migrations successfully executed');
+      process.exit(0);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
 
 try {
